cb1434d269c1295bb154f3fc2ac133f6f6741ab1
Rachel Ferretto <rachel.ferretto@rea-group.com>
2018-07-24 19:43:26 +1000

    add login ability

--- app_console.rb
+++ app_console.rb
@@ -4,3 +4,6 @@ require_relative 'models/user'
 require_relative 'models/exercise'
 require_relative 'models/like'
 require_relative 'models/comment'
+
+
+binding.pry
--- main.rb
+++ main.rb
@@ -11,7 +11,23 @@ require_relative 'models/exercise'
 require_relative 'models/like'
 require_relative 'models/comment'
 
+enable :sessions
 
+helpers do 
+  def current_user 
+    User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    if current_user
+      true
+    else
+      false
+    end
+
+  end
+
+end
 
 get '/' do
   @exercises = Exercise.all
@@ -45,6 +61,7 @@ get '/exercises/:id' do
 end
 
 delete '/exercises/:id' do
+  redirect '/login' unless logged_in?
   exercise = Exercise.find(params[:id])
   exercise.destroy
   redirect '/'
@@ -70,6 +87,7 @@ end
 
 
 post '/comments' do
+  redirect '/login' unless logged_in?
   comment = Comment.new
   comment.content = params[:content]
   comment.exercise_id = params[:exercise_id]
@@ -84,6 +102,34 @@ delete '/comments/:id' do
   redirect "/"
 end
 
+post '/likes' do
+  like = Like.new
+  like.dish_id = params[:dish_id]
+  like.user_id = current_user.id
+  like.save
+
+  redirect "/dishes/#{params[:dish_id]}"
+end
+
+
+get '/login' do 
+  erb :login
+end
+
+post '/session' do
+  user = User.find_by(email: params[:email])
+  if user && user.authenticate(params[:password])
+   session[:user_id] = user.id
+    redirect '/'
+  else 
+    erb :login
+  end
+end
+
+delete '/session' do
+  session[:user_id] = nil
+  redirect '/login'
+end
 
 
 
